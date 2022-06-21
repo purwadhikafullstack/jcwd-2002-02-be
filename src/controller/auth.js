ef1e613c02eac16bd2c51ae830a5d1ba3adea2e2
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-06-21 16:11:40 +0700

    Feat: added verifyUser service

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -158,6 +158,23 @@ const authController = {
       });
     }
   },
+
+  verifyUser: async (req, res) => {
+    try {
+      const { token } = req.params;
+
+      const serviceResult = await AuthService.verifyUser(token);
+
+      if (!serviceResult.success) throw serviceResult;
+
+      return res.redirect(serviceResult.url);
+    } catch (err) {
+      console.log(err);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
+    }
+  },
 };
 
 module.exports = authController;
