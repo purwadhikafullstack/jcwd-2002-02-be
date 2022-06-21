329d00352ba221926e06c8e529d8feec9aafb737
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-06-21 19:51:11 +0700

    Feat: added resend verification end point

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,8 +1,14 @@
 const authController = require("../controller/auth");
+const { authorizedLoginUser } = require("../middleware/authorizeLoginUser");
 
 const router = require("express").Router();
 
 router.post("/register", authController.registerUser);
 router.get("/verify/:token", authController.verifyUser);
+router.post(
+  "/resend-verification-email",
+  authorizedLoginUser,
+  authController.resendVerificationEmail
+);
 
 module.exports = router;
