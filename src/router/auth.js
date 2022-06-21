8c9e5998aea3cad71ece359ef59f50536848b183
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-06-21 16:11:40 +0700

    Feat: added verifyUser service

--- src/router/auth.js
+++ src/router/auth.js
@@ -3,5 +3,6 @@ const authController = require("../controller/auth");
 const router = require("express").Router();
 
 router.post("/register", authController.registerUser);
+router.get("/verify/:token", authController.verifyUser);
 
 module.exports = router;
--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -213,6 +213,106 @@ class AuthService extends Service {
       });
     }
   };
+
+  static verifyUser = async (token) => {
+    try {
+      const verifyToken = await AccountVerificationToken.findOne({
+        where: {
+          token,
+          is_valid: true,
+          valid_until: {
+            [Op.gt]: moment().utc(),
+          },
+        },
+      });
+
+      if (!verifyToken) {
+        return this.handleError({
+          message: "Token is not valid!",
+          statusCode: 401,
+        });
+      }
+
+      await User.update(
+        { is_verified: true },
+        {
+          where: {
+            id: verifyToken.userId,
+          },
+        }
+      );
+
+      await AccountVerificationToken.update(
+        { is_valid: false },
+        {
+          where: {
+            userId: verifyToken.userId,
+          },
+        }
+      );
+
+      return this.handleRedirect({
+        url: `http://localhost:3000/verifikasi-berhasil?referral=${token}`,
+      });
+    } catch (err) {
+      return this.handleError({
+        message: "Server Error!",
+        statusCode: 500,
+      });
+    }
+  };
+
+  static resendVerificationToken = async (req) => {
+    try {
+      const userId = req.token.id;
+
+      const findUser = await User.findByPk(userId);
+
+      if (findUser.is_verified) {
+        return this.handleError({
+          message: "Your account has been verified",
+          statusCode: "400",
+        });
+      }
+
+      const verifyAccountToken = nanoid(40);
+
+      await AccountVerificationToken.create({
+        token: verifyAccountToken,
+        is_valid: true,
+        valid_until: moment().add(1, "hour"),
+        userId: findUser.id,
+      });
+
+      const verifyUserLink = `http://localhost:2000/auth/verify/${verifyAccountToken}`;
+
+      const emailTemplate = fs
+        .readFileSync(__dirname + "/../../templates/verifyAccount.html")
+        .toString();
+
+      const renderedTemplate = mustache.render(emailTemplate, {
+        name: findUser.nama,
+        verify_url: verifyUserLink,
+      });
+
+      await mailer({
+        subject: "Verfiy your account!",
+        to: findUser.email,
+        html: renderedTemplate,
+      });
+
+      return this.handleSuccess({
+        message: "Verification link has been sent!",
+        statusCode: 200,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Server error!",
+        statusCode: 500,
+      });
+    }
+  };
 }
 
 module.exports = AuthService;
